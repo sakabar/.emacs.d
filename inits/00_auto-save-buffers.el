2ae118d5e38086f9516366c944bdf76e1fad3418
Takafumi Sakakibara <tsakaki@lr.pi.titech.ac.jp>
2016-12-08 21:02:12 +0900

    Modify: auto save timer

--- inits/00_auto-save-buffers.el
+++ inits/00_auto-save-buffers.el
@@ -1,4 +1,4 @@
 (require 'auto-save-buffers)
-(run-with-idle-timer 5 t 'auto-save-buffers)
+(run-with-idle-timer 3 t 'auto-save-buffers)
 ;; (define-key ctl-x-map "as" 'auto-save-buffers-toggle)
 ;; (global-set-key "\C-x\C-s" 'auto-save-buffers-toggle)
