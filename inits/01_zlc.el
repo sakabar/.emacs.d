bdd0d64bd6a885bfa14d4015bfaca5dc1f316608
Takafumi Sakakibara <tsakaki@lr.pi.titech.ac.jp>
2016-12-08 21:02:43 +0900

    Modify: not use zlc

--- inits/01_zlc.el
+++ inits/01_zlc.el
@@ -1,14 +1,17 @@
-(require 'zlc)
-(zlc-mode 1)
-(setq zlc-select-completion-immediately t)
+;; 1回タブを押した時に1番目のファイルではなく../が補完される。直す方法が分からないので使うのをやめる。
 
-(let ((map minibuffer-local-map))
-  ;;; like menu select
-  ;; (define-key map (kbd "<down>")  'zlc-select-next-vertical)
-  ;; (define-key map (kbd "<up>")    'zlc-select-previous-vertical)
-  ;; (define-key map (kbd "<right>") 'zlc-select-next)
-  ;; (define-key map (kbd "<left>")  'zlc-select-previous)
+;; (require 'zlc)
+;; (zlc-mode 1)
+;; (setq zlc-select-completion-immediately t)
 
-  ;;; reset selection
-  (define-key map (kbd "C-c") 'zlc-reset)
-  )
+;; (let ((map minibuffer-local-map))
+;;   ;;; like menu select
+;;   ;; (define-key map (kbd "<down>")  'zlc-select-next-vertical)
+;;   ;; (define-key map (kbd "<up>")    'zlc-select-previous-vertical)
+;;   ;; (define-key map (kbd "<right>") 'zlc-select-next)
+;;   ;; (define-key map (kbd "<left>")  'zlc-select-previous)
+
+;;   ;;; reset selection
+;;   (define-key map (kbd "C-c") 'zlc-reset)
+;;   )
+
